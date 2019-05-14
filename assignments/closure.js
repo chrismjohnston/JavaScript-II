36a2c4fe2de0034eabd36369286b8ecc748fced4
Chris Johnston <cmjohnston@gmail.com>
2019-05-14 17:35:11 -0500

    closure done with stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -15,16 +15,35 @@ say2();
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = (lambda = 0) => {
   // Return a function that when invoked increments and returns a counter variable.
+  let value = lambda;
+  return function counter(){
+  value += 1;
+  console.log(value);
+  return value;
+}
 };
+
+const newCounter = counter();
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+newCounter(); // 1
+newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let counter = 0;
+  console.log(counter);
+  let increase = () => counter += 1;
+  let decrease = () => counter -= 1;
+  let resObj = {increase , decrease};
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  return resObj;
 };
+
+let dino = counterFactory();
+
+console.log(dino.increase())
+console.log(dino.decrease())
