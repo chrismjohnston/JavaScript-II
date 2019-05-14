0d7eb6f269c9ce0b9863ea1e2444fafa6ba34c59
Chris Johnston <cmjohnston@gmail.com>
2019-05-14 17:43:11 -0500

    done for tonight

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -80,8 +80,8 @@ contains('Pencil' ,items, function(contains){
 
 /* STRETCH PROBLEM */
 
-// function removeDuplicates(array, cb) {
+function removeDuplicates(array, cb) {
 //   // removeDuplicates removes all duplicate values from the given array.
 //   // Pass the duplicate free array to the callback function.
 //   // Do not mutate the original array.
-// }
+}
